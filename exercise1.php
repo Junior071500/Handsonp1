0e10034e4f3080bf71a7220e750d97b442ae4219
Felipe <felipesabit57@gmail.com>
2022-10-13 23:16:56 +0800

    first commit

---
+++ exercise1.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Twinkle, Twinkle little star";
+
+$x = "Twinkle";
+$y ="Star";
+
+echo "\n". $x. ",Twinkle little " . $y;
+
+?>
---
+++ exercise4.php
@@ -0,0 +1,14 @@
+?php   
+ 
+$name='Harry'; 
+$age=28;   
+var_dump($name); 
+echo "";   
+print_r($name); 
+echo "\n";   
+var_dump($age); 
+echo "";  
+$name=NULL;   
+var_dump($name);
+
+?>
